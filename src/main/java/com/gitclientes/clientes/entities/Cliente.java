a6edb7b376299703dd68b30fa7e9903519a7b691
Ismael Felix <ismael.duarte017@gmail.com>
2021-05-28 20:32:55 -0300

    created customer entity

---
+++ src/main/java/com/gitclientes/clientes/entities/Cliente.java
@@ -0,0 +1,24 @@
+package com.gitclientes.clientes.entities;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false, length = 100)
+    private String name;
+    @Column(nullable = false, length = 11)
+    private String cpf;
+    @Column
+    private LocalDate dateRegiser;
+
+}
