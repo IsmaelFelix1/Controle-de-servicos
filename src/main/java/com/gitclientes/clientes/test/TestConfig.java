a0fe503c4100361215a4e511443c84096cb2dced
Ismael Felix <ismael.duarte017@gmail.com>
2021-05-29 10:02:14 -0300

    created class TestConfig

---
+++ src/main/java/com/gitclientes/clientes/test/TestConfig.java
@@ -0,0 +1,24 @@
+package com.gitclientes.clientes.test;
+
+import com.gitclientes.clientes.entities.Cliente;
+import com.gitclientes.clientes.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Cliente c1 = new Cliente(null, "Ismael", "00000000000", null);
+
+        clienteRepository.saveAll(Arrays.asList(c1));
+    }
+}
