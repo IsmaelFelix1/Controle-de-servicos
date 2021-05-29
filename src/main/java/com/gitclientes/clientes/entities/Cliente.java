e1f6cc76b82f3865497341aeeb1d8f05542a3e2c
Ismael Felix <ismael.duarte017@gmail.com>
2021-05-29 12:03:37 -0300

    Format date

--- src/main/java/com/gitclientes/clientes/entities/Cliente.java
+++ src/main/java/com/gitclientes/clientes/entities/Cliente.java
@@ -1,15 +1,17 @@
 package com.gitclientes.clientes.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Cliente {
+public class Cliente implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +21,7 @@ public class Cliente {
     @Column(nullable = false, length = 11)
     private String cpf;
     @Column(name = "date_register")
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dateRegiser;
 
     @PrePersist
